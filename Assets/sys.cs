4b6c04f0536d53baeca9ba47803cb031165003cb
Amine <aminenekrouche05@gmail.com>
2026-04-09 15:27:53 +0100

    progress

--- Assets/sys.cs
+++ Assets/sys.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 
 public class sys : MonoBehaviour
 {
     public string[] cards,cards_shuffled = new string[40];
-    char[] signs = {'d','a','s','h'};
+
+    char[] signs = {'o','c','e','b'};
+
+    [SerializeField] TextMeshProUGUI card_display, player1Hand, player2Hand;
+
+    struct player
+    {
+        public bool isHere;
+        public int score;
+        public string[] cards;
+    }
+
+    player player1, player2;
     void Start(){
         StartCoroutine(fillAndShuffle());
+    }
+    void Update()
+    {
+        
     }
 
     IEnumerator fillAndShuffle()
@@ -32,5 +49,30 @@ public class sys : MonoBehaviour
             cards_shuffled[t] = cards_list[rand];
             cards_list.Remove(cards_list[rand]);
         }
+
+        for(int k = 0; k < cards_shuffled.Length; k++)
+        {
+            card_display.text += cards_shuffled[k] + ", ";
+        }
+        StartCoroutine(StartGame());
+    }
+
+    IEnumerator StartGame()
+    {
+        // wait until players join
+        player1.isHere = true;
+        player2.isHere = true;
+        int i;
+        for (i = 0; i < 5;i++)
+        {
+            player1.cards[i] = cards_shuffled[i];
+            player1Hand.text += player1.cards[i] + " ";
+            yield return new WaitForSeconds(0.5f);
+            player2.cards[i] = cards_shuffled[i+1];
+            player2Hand.text += player2.cards[i] + " ";
+            yield return new WaitForSeconds(1f);
+        }
+        
+        yield return null;
     }
 }
